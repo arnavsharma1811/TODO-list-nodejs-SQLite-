e337fb79f2ba245047eed03073f6e148a99c6822
arnavsharma1811 <arnavsharmaone8@gmail.com>
2025-11-16 00:55:35 +0530

    Did the initial setUp for registeration of user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,6 +7,19 @@ const router = express.Router()
 
 router.post('/register', (req, res) =>{
 
+    const {username, password} = req.body
+
+
+    const hashedPassword = bcrypt.hashSync(password, 8)
+    try{
+        const insertUser = db.prepare(`INSERT INTO users (username, password)
+            VALUES(?,?) `)
+            const result = insertUser.run(username, hashedPassword)
+    }
+    catch(err){
+       console.log(err.message)
+       res.sendStatus(503) 
+    }
 })
 
 router.post('/login', (req,res)=>{
