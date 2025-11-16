3e52ff740ac159ed923d679f95b96f49ddf55f78
arnavsharma1811 <arnavsharmaone8@gmail.com>
2025-11-17 01:54:57 +0530

    final commit finished it with setting up the endpoints and remeaning routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,6 +2,8 @@ import express from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import db from '../db.js'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const router = express.Router()
 
@@ -15,6 +17,16 @@ router.post('/register', (req, res) =>{
         const insertUser = db.prepare(`INSERT INTO users (username, password)
             VALUES(?,?) `)
             const result = insertUser.run(username, hashedPassword)
+
+            const defaultTodo = `Hello :) Add your first todo!`
+            const insertTodo = db.prepare(`INSERT INTO todos (user_id , task)
+                VALUES(?,?)`)
+                insertTodo.run(result.lastInsertRowid, defaultTodo);
+                   
+                const token = jwt.sign({id: result.lastInsertRowid}, process.env.JWT_SECRET, {expiresIn: '24h'})
+                    
+                    res.json({token})
+
     }
     catch(err){
        console.log(err.message)
@@ -23,7 +35,25 @@ router.post('/register', (req, res) =>{
 })
 
 router.post('/login', (req,res)=>{
+    const{username, password} = req.body;
+    try{
     
+        const getUser = db.prepare(`SELECT * FROM users WHERE username = ? `)
+    const user = getUser.get(username)
+    if(!user){return res.status(404).send({message: "User not found"})}
+    const passwordIsValid = bcrypt.compareSync(password, user.password)
+    if(!passwordIsValid){
+        return res.sendStatus(401).send({message: "Invalid password"})
+    }
+    const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {
+        expiresIn:'24h'
+    })  
+    res.json({token})
+}
+catch(err){
+    console.log(err.message)
+    res.sendStatus(503)
+} 
 } )
 
 export default router 
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -3,9 +3,36 @@ import db from '../db.js'
 
 const router = express.Router()
 
-router.get('/', (req,res)=> {})
-router.post('/', (req,res)=> {})
-router.put('/:id', (req,res)=> {})
-router.delete('/:id', (req,res)=> {})
+router.get('/', (req,res)=> {
+    const getTodos = db.prepare(`SELECT * FROM todos WHERE user_id = ?`)
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
+})
+router.post('/', (req,res)=> {
+    const {task} = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) 
+        VALUES(?,?)`)
+        const result = insertTodo.run(req.userId, task)
+
+        res.json({ id: result.lastInsertRowid, task, completed: 0})
+})
+router.put('/:id', (req,res)=> {
+const {completed} = req.body
+const {id} = req.params
+const {page} = req.query
+
+const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+updatedTodo.run(completed, id)
+
+res.json({message: "Todo completed"})
+
+})
+router.delete('/:id', (req,res)=> {
+    const userId = req.userId
+    const {id} = req.params
+    
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id, userId)
+})
 
 export default router 
